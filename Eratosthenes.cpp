f1e380d28e44a85aa3838e34bc5ee3ed5f9df0b9
CarryJoe <CarryJoe@users.noreply.github.com>
2018-10-22 20:50:36 +0800

    Add files via upload

---
+++ Eratosthenes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#define MaxSize 1000
+int main()
+{
+	bool a[MaxSize];
+	int i,k,j;
+	for(i=2;i<MaxSize;i++)
+	{
+		a[i]=true;            //打上素数的标记
+	}
+	a[0]=a[1]=false;
+	i=2;
+	while(i<MaxSize)
+	{
+		if(a[i]==true)
+		{
+		  k=2;
+		  while(i*k<MaxSize)
+		  {
+			  a[i*k]=false;
+			  k++;
+		  }
+		}
+		i++;
+	}
+	for(i=1;i<MaxSize;i++)
+	{
+		if(a[i])cout<<i<<" ";
+	}
+	cout<<endl;
+	cin>>i;
+	return 0;
+}
